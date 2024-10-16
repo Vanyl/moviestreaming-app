25fa414de25e356d11627c7b3a00b623172697c8
vanyl <yavannaperez@hotmail.com>
2024-10-17 01:18:22 +0200

    cursor pointer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
             <footer className="flex flex-col p-6 items-center justify-center ">
             <img className="max-h-[32px] max-w-[110px] px-3" src={amazonPrimeLogo} alt="Prime Video" />
             <ul className="text-blue-300 list-none font-semibold flex justify-center gap-x-4">
-                <li className="hover:text-blue-200">Conditions d'utilisations et Politique de confidentialité</li>
-                <li className="hover:text-blue-200">Donnez-nous votre avis</li>
-                <li className="hover:text-blue-200">Aide</li>
-                <li className="hover:text-blue-200">Avertissement relatif aux cookies</li>
+                <li className="hover:text-blue-200 cursor-pointer">Conditions d'utilisations et Politique de confidentialité</li>
+                <li className="hover:text-blue-200 cursor-pointer">Donnez-nous votre avis</li>
+                <li className="hover:text-blue-200 cursor-pointer">Aide</li>
+                <li className="hover:text-blue-200 cursor-pointer">Avertissement relatif aux cookies</li>
                 <li className="text-gray-600">&#169; 1996-2024, Amazon.com, Inc ou ses filiales</li>
             </ul>
             </footer>
