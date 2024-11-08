5b6bafa252570256699006332cd03ce20f410da0
vanyl <yavannaperez@hotmail.com>
2024-11-08 14:47:25 +0100

    carousel mobile fixed + refacto

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,6 +9,7 @@ const CarouselDefault = ({ api }) => {
   const img_1280 = 'https://image.tmdb.org/t/p/w1280'
   const unavailable = 'https://www.movienewz.com/img/films/poster-holder.jpg'
   const [items, setItems] = useState([]);
+  const [isMobile, setIsMobile] = useState(false);
 
   const fetchLogo = async (id, mediaType) => {
     const response = await fetch(`https://api.themoviedb.org/3/${mediaType}/${id}/images?api_key=${process.env.REACT_APP_TMDB_API_KEY}&include_image_language=en,null`);
@@ -28,13 +29,24 @@ const CarouselDefault = ({ api }) => {
       }
       const dataJson = await response.json();
 
-      const itemsWithLogos = await Promise.all(
-        dataJson.results.map(async (item) => {
-          const mediaType = item.media_type || (api.includes('/movie') ? 'movie' : 'tv');
-          const logo = await fetchLogo(item.id, mediaType);
-          return { ...item, media_type: mediaType, logo }; 
-        })
-      );
+      let itemsWithLogos;
+
+      // fetching a single movie
+      if (dataJson.id) {
+        const mediaType = api.includes("/movie") ? "movie" : "tv";
+        const logo = await fetchLogo(dataJson.id, mediaType);
+        itemsWithLogos = [{ ...dataJson, logo }];
+      } else {
+        // fetching multiple items
+        itemsWithLogos = await Promise.all(
+          dataJson.results.map(async (item) => {
+            const mediaType = item.media_type || (api.includes("/movie") ? "movie" : "tv");
+            const logo = await fetchLogo(item.id, mediaType);
+            return { ...item, media_type: mediaType, logo };
+          })
+        );
+      }
+
       setItems(itemsWithLogos);
     } catch (error) {
       console.error(error);
@@ -52,17 +64,26 @@ const CarouselDefault = ({ api }) => {
     return text;
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    }
+    handleResize(); 
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  },[])
+
 
   return (
     <Carousel
       transition={{ duration: 1, type: "tween" }}
       className="h-5/6 w-screen rounded-lg overflow-hidden mb-[48px]"
       navigation={({ setActiveIndex, activeIndex, length }) => (
-        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-          {new Array(length).fill("").map((_, i) => (
+        <div className="absolute bottom-4 z-50 flex justify-center w-full gap-2">
+          {Array.from({ length }, (_, i) => (
             <span
               key={i}
-              className={`block h-1.5 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-3.5 bg-white" : "w-1.5 bg-white/50"
+              className={`h-1.5 cursor-pointer rounded-full ${activeIndex === i ? "w-3.5 bg-white" : "w-1.5 bg-white/50"
                 }`}
               onClick={() => setActiveIndex(i)}
             />
@@ -71,12 +92,14 @@ const CarouselDefault = ({ api }) => {
       )}
     >
       {items.slice(0, 5).map((item) => {
-        const { id, title, name, original_name, backdrop_path, overview, logo } = item;
+        const { id, title, name, original_name, backdrop_path, poster_path, overview, logo } = item;
         return (
           <div key={id} className="relative h-full w-full">
             <img
-              src={backdrop_path ? `${img_1280}/${backdrop_path}` : unavailable}
-              alt={item.title || item.original_name}
+              src={
+                isMobile ? poster_path ? `${img_1280}/${poster_path}` : unavailable : backdrop_path ? `${img_1280}/${backdrop_path}` : unavailable
+              }
+              alt={title || original_name}
               className="object-cover h-auto w-full"
             />
             <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
@@ -90,8 +113,8 @@ const CarouselDefault = ({ api }) => {
                   {logo && logo !== unavailable ? (
                     <img
                       src={logo}
-                      alt={item.media_type === 'movie' ? item.title : item.name}
-                      className="h-10 w-auto object-contain"
+                      alt={title || original_name}
+                      className=" h-[10vw] md:h-[15vw]w-auto object-contain"
                     />
                   ) : (
                     title ? title : original_name
