11adad96baaa137a82ee05add78370df1d9a210a
vanyl <yavannaperez@hotmail.com>
2024-11-08 14:51:44 +0100

    refacto

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -1,76 +1,55 @@
 import { useState, useEffect } from "react";
-const apiKey = 'fe367ab8576243891c127d4f54eb4982';
- 
-const Genres = ({ genre, setGenre, type, value, setValue }) => {
-const fetchGenre = async () => {
-const data = await fetch( `https://api.themoviedb.org/3/genre/${type}/list?api_key=${apiKey}&language=en-US
-`);
-const { genres } = await data.json();
-setGenre(genres);
-};
- 
-useEffect(() => {
-fetchGenre();
-}, []);
+import Card from "./Card";
+import Slider from "./Slider";
 
-//Adding a particular genre to the selected array
-const CategoryAdd = (genres) => {
-    //first - select everything that's inside of values using the spread operator
-    //second - add those genres that are being sent from the non-selected arrays
-    setValue([...value, genres]);
-    //removing those genres from the non-selected array that have been added to the selected array.
-    setGenre(genre.filter((g) => g.id !== genres.id));
-};
+const Genres = ({ media }) => {
+  const [genres, setGenres] = useState([]);
+  const [genreItems, setGenreItems] = useState({});
+  const img_780 = 'https://image.tmdb.org/t/p/w780'
 
-//removing a particular genre from the selected array
-const CategoryRemove = (genres) => {
-    setValue(value.filter((g) => g.id !== genres.id));
-    setGenre([...genre, genres]);
-    };
+  const fetchMediaGenre = async () => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/genre/${media}/list?api_key=${process.env.REACT_APP_TMDB_API_KEY}`);
+      const { genres } = await response.json();
+      setGenres(genres);
 
-    return(
+      // fetch movies for each genre after getting the list
+      genres.forEach(async (genre) => {
+        const genreResponse = await fetch(`https://api.themoviedb.org/3/discover/${media}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=${genre.id}`);
+        const genreData = await genreResponse.json();
+        setGenreItems((prevItems) => ({
+          ...prevItems,
+          [genre.id]: genreData.results,
+        }));
+      });
+    } catch (error) {
+      console.error(`Error fetching ${media} genres or items:`, error);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchMediaGenre()
+  }, [media]);
+
+  return (
     <>
-      <div className="genre-container">
-        <div className="row mb-3">
-          <div className="col-12 d-flex flex-wrap">
-            {value &&
-              value.map((Val) => {
-                const { id, name } = Val;
-                return (
-                  <>
-                    <div className="m-2" key={id}>
-                      <button
-                        className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:scale-125 focus:outline-none focus:bg-gray-700"
-                        onClick={() => CategoryRemove(Val)}
-                      >
-                        {name}
-                      </button>
-                    </div>
-                  </>
-                );
-              })}
-            {genre && //if genre exist
-              genre.map((Gen) => {
-                const { id, name } = Gen;
-                return (
-                  <>
-                    <div className="m-2" key={id}>
-                      <button
-                        className="bg-gray-800 text-white px-4 py-2 rounded-lg  hover:scale-125 focus:outline-none focus:bg-gray-700"
-                        onClick={() => CategoryAdd(Gen)}
-                      >
-                        {name}
-                      </button>
-                    </div>
-                  </>
-                );
-              })}
-
+      <div className="genre-sections">
+        {genres.map((genre) => (
+          <div key={genre.id} className="genre-section mb-10">
+            <h2 className="text-lg text-white font-bold">{genre.name} - {media === "movie" ? "Movies" : "TV Shows"}</h2>
+            <Slider
+            items={genreItems[genre.id] || []}
+            itemsPerPage={4}
+            render={(item) => (
+              <Card key={item.id} item={item} img_780={img_780} media={media}/>
+            )}
+          />
           </div>
-        </div>
+        ))}
       </div>
-      </>
-      )
+    </>
+  )
 }
 
 
